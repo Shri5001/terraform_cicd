f298e525edf86f2a7e25fe9e9549b2a22438be90
Shrikrishna Yeole <shriyeole5001@gmail.com>
2024-09-04 11:43:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-provider "aws" {
+provider "aws" { 
+region = "us-east-1"
 
 }
 
-resource "aws_instance" "ec2-instance" {
+resource "aws_instance" "ec2" {
     ami = "ami-02c21308fed24a8ab"
     instance_type = "t2.micro"
     tags = {
