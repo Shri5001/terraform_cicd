fa118f6a1c2180d72fb8ad1aaa44780e223f8489
Shrikrishna Yeole <shriyeole5001@gmail.com>
2024-09-04 11:41:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2-instance" {
-    ami = "ami-066784287e358dad1"
+    ami = "ami-02c21308fed24a8ab"
     instance_type = "t2.micro"
     tags = {
       Name = "dev-ec2"
