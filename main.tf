c438125415c9138f2a204cf67cf047abf453fc02
Shrikrishna Yeole <shriyeole5001@gmail.com>
2024-09-09 17:26:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "ec2" {
-    ami = "ami-02c21308fed24a8ab"
+    ami = "ami-0182f373e66f89c85"
     instance_type = "t2.micro"
     tags = {
-      Name = "deve"
+      Name = "dev"
     }
 }
